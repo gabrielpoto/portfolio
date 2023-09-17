b8927bdad9579c284d264b305b827f55f11ef0ef
gabrielpoto <potogabriel1@gmail.com>
2023-09-17 19:18:24 -0400

    change language fr by default

--- src/traduction/i18n.js
+++ src/traduction/i18n.js
@@ -10,8 +10,8 @@ i18n
             en: { translation: enTranslation },
             fr: { translation: frTranslation }
         },
-        lng: 'en', // Langue par défaut
-        fallbackLng: 'en', // Langue de secours si la langue souhaitée n'est pas disponible
+        lng: 'fr', // Langue par défaut
+        fallbackLng: 'fr', // Langue de secours si la langue souhaitée n'est pas disponible
         interpolation: {
             escapeValue: false // Permet d'ajouter des balises HTML dans les traductions
         }
